ebd6e459f652ef6bc9cf81d6adeaa3e18c93c72c
kimseulbi <ksb5166@gmail.com>
2019-10-01 13:51:49 +0900

    Layout flex로 구성

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.yellowView}>
+        <Text>Hello</Text>
+      </View>
+      <View style={styles.blueView}>
+        <Text>Hello</Text>
+      </View>
     </View>
   );
 }
@@ -12,8 +16,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  yellowView: {
+    flex: 1,
+    backgroundColor: "yellow",
     alignItems: 'center',
     justifyContent: 'center',
   },
+  blueView: {
+    flex: 3,
+    backgroundColor: "blue",
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
